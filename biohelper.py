678d383dbbbc07397670507cfff541eedfe3d564
Scrooge <76687908+SkrudjReal@users.noreply.github.com>
2022-11-13 14:33:32 +0100

    Update biohelper.py

--- biohelper.py
+++ biohelper.py
@@ -8,7 +8,7 @@ import telethon
 from telethon import types
 from telethon.tl.types import Message
 
-logger = logging.getLogger(__name__)
+#logger = logging.getLogger(__name__)
 
 @loader.tds
 class heal(loader.Module):
